07e49504ede0346248f058468bb98a499fd96fa8
Sergey <inferpse@users.noreply.github.com>
2017-07-31 23:09:34 +0300

    added source-map support

--- index.js
+++ index.js
@@ -1,82 +1,154 @@
-var RawSource = require('webpack-sources').RawSource,
-    path = require("path"),
-    cwd = process.cwd();
+const { SourceMapSource, RawSource } = require('webpack-sources'),
+      babel = require('babel-core'),
+      path = require('path'),
+      cwd = process.cwd();
 
-function InnerPromisePlugin(options) {
-  this.options = options || {
-    modulePath: '',
-    moduleAccessor: ''
-  };
+class InnerPromisePlugin {
+  constructor(options) {
+    this.options = Object.assign({
+      jsregex: /\.js($|\?)/i,
+      modulePath: '',
+      moduleAccessor: ''
+    }, options);
+  }
+  apply(compiler) {
+    const { options } = this,
+          { jsregex } = options,
+          modulePath = path.resolve(cwd, options.modulePath),
+          useSourceMap = typeof options.sourceMap === 'undefined' ? !!compiler.options.devtool : options.sourceMap;
+
+    compiler.plugin('compilation', function (compilation) {
+
+      if (useSourceMap) {
+        compilation.plugin('build-module', function (module) {
+          module.useSourceMap = true;
+        });
+      }
+
+      compilation.plugin('optimize-chunk-assets', function (chunks, callback) {
+        let files = [],
+            moduleId = null;
+
+        chunks.forEach(chunk => {
+          // find local module with Promise implementation
+          chunk.forEachModule(function(module) {
+            if (module.fileDependencies && moduleId === null) {
+              module.fileDependencies.forEach(function(filePath) {
+                if (moduleId === null && filePath === modulePath) {
+                  moduleId = module.id;
+                }
+              });
+            }
+          });
+
+          chunk.files.forEach(file => files.push(file));
+        });
+
+        compilation.additionalChunkAssets.forEach(file => files.push(file));
+
+        // do nothing if we were unable to find local promise implementation
+        if (moduleId === null) {
+          compilation.errors.push(new Error('InnerPromisePlugin: module with promise implementation not found!'));
+          callback();
+          return;
+        }
+
+        // save reference to module id in options
+        options.moduleId = moduleId;
+
+        files.filter(file => jsregex.test(file)).forEach(file => {
+          try {
+            let asset = compilation.assets[file];
+
+            // use cached asset
+            if (asset.__innerPromiseApplied) {
+              compilation.assets[file] = asset.__innerPromiseApplied;
+              return;
+            }
+
+            // read options
+            let input, inputSourceMap;
+            if (useSourceMap) {
+              if (asset.sourceAndMap) {
+                let sourceAndMap = asset.sourceAndMap();
+                inputSourceMap = sourceAndMap.map;
+                input = sourceAndMap.source;
+              } else {
+                inputSourceMap = asset.map();
+                input = asset.source();
+              }
+            } else {
+              input = asset.source();
+            }
+
+            // apply transformation
+            const result = babel.transform(input, {
+              plugins: [
+                [InjectLocalPromise, options]
+              ],
+              sourceMaps: useSourceMap,
+              compact: false,
+              babelrc: false,
+              inputSourceMap
+            });
+
+            // save result
+            asset.__innerPromiseApplied = compilation.assets[file] = (
+              result.map
+              ? new SourceMapSource(result.code, file, result.map, input, inputSourceMap)
+              : new RawSource(result.code)
+            );
+          } catch (e) {
+            compilation.errors.push(e);
+          }
+        });
+
+        callback();
+      })
+    });
+  }
 }
 
-InnerPromisePlugin.prototype.apply = function(compiler) {
-  var options = this.options;
-  var jsregex = options.test || /\.js($|\?)/i;
-  var modulePath = path.resolve(cwd, options.modulePath);
-
-  compiler.plugin('compilation', function (compilation) {
-    compilation.plugin('optimize-chunk-assets', function (chunks, callback) {
-      var moduleId = null;
-      const files = [];
-
-      chunks.forEach(function(chunk) {
-        chunk.modules.forEach(function(module) {
-          if (module.fileDependencies && moduleId === null) {
-            module.fileDependencies.forEach(function(filePath) {
-              if (moduleId === null && filePath === modulePath) {
-                moduleId = module.id;
-              }
-            });
-          }
-        });
-
-        chunk.files.forEach(function(file) {
-          files.push(file);
-        });
-      });
-
-      compilation.additionalChunkAssets.forEach(function(file) {
-        files.push(file);
-      });
-
-      files.filter(function(file) {
-        return jsregex.test(file);
-      }).forEach(function(file) {
-        try {
-          var asset = compilation.assets[file];
-
-          // return cached version
-          if (asset.__innerPromiseApplied) {
-            compilation.assets[file] = asset.__innerPromiseApplied;
-            return;
-          }
-
-          // grab source code
-          var input = asset.source();
-
-          // make code es3 compatible
-          var result = input.replace(/__webpack_require__\.bind\(null, (.*?)\)/, 'function(){ return __webpack_require__($1) }');
-
-          // inject custom promise implementation (if moduleId was found) and make code ES3 compatible
-          if (moduleId !== null) {
-            var customPromiseInclude = 'var Promise = __webpack_require__(' + moduleId + ')' + (options.moduleAccessor) + ';';
-            result = result
-                      .replace(/(__webpack_require__\.e.*?{)/, '$1 ' + customPromiseInclude)
-                      .replace(/(function webpackAsyncContext.*?{)/, '$1 ' + customPromiseInclude)
-                      .replace(/(Promise\.all\(ids\.slice\(1\)\.map\(__webpack_require__\.e\)\))/g, 'Promise.all(function(){ var r = ids.slice(1); for(var i = 0; i < r.length; i++) { r[i] = __webpack_require__.e(r[i]); } return r; }())')
-                      .replace(/return Object\.keys\(map\);/g, 'return (function() { var r = []; for (var p in map) { if (map.hasOwnProperty(p)) { r.push(p); } } return r; }())');
-          }
-
-          // save result
-          asset.__innerPromiseApplied = compilation.assets[file] = new RawSource(result);
-        } catch(e) {
-          compilation.errors.push(e);
+const InjectLocalPromise = ({types: t}) => {
+  return {
+    visitor: {
+      Identifier: (path, {opts: options}) => {
+        const { node, scope } = path;
+        if (isWebpackInternalPromise(path)) {
+          replacePromiseIdentifier(path, options);
         }
-      });
+      }
+    }
+  };
 
-      callback();
+  function isWebpackInternalPromise(path) {
+    const { node, parentPath } = path;
+    if (node.name === 'Promise') {
+      const parentFunc = getParentFunction(path),
+            parentNode = parentFunc && parentFunc.node;
+      return parentFunc && (
+        (parentNode.type === 'FunctionDeclaration' && parentNode.id && parentNode.id.name === 'webpackAsyncContext')
+        ||
+        (parentNode.type === 'FunctionExpression' && parentNode.id && parentNode.id.name === 'requireEnsure')
+      )
+    }
+  }
+
+  function getParentFunction(path) {
+    return path.findParent(path => {
+      return path.isFunctionExpression() || path.isFunctionDeclaration();
     });
-  });
-};
+  }
+
+  function replacePromiseIdentifier(path, options) {
+    let replacement = t.callExpression(t.identifier('__webpack_require__'), [t.numericLiteral(options.moduleId)]);
+
+    if (options.moduleAccessor) {
+      replacement = t.memberExpression(replacement, t.identifier(options.moduleAccessor))
+    }
+
+    path.replaceWith(replacement);
+  }
+}
 
 module.exports = InnerPromisePlugin;
